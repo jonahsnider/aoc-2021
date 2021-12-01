92a0184f8953473c6c378a52c70519901a5aa4e8
Jonah Snider <jonah@jonahsnider.com>
2021-11-30 21:59:54 -0800

    perf(day1): clean up

--- src/days/implementations/1/index.ts
+++ src/days/implementations/1/index.ts
@@ -11,24 +11,25 @@ export class Day1 extends Day {
 		for (let i = 1; i < numbers.length; i++) {
 			const a = numbers[i];
 			const b = numbers[i - 1];
-			const c = numbers[i - 2];
-			const d = numbers[i - 3];
 
 			if (a > b) {
 				solution[0]++;
 			}
 
-			if (a && b && c && d) {
-				const previousSum = b + c + d;
-				const curSum = a + b + c;
+			if (i > 2) {
+				const c = numbers[i - 2];
+				const d = numbers[i - 3];
 
-				if (curSum > previousSum) {
+				const sharedSum = b + c;
+				const previousSum = sharedSum + d;
+				const currentSum = a + sharedSum;
+
+				if (currentSum > previousSum) {
 					solution[1]++;
 				}
 			}
 		}
 
-		// This should only happen when there is bad input
 		return solution;
 	}
 }

b99005b2ab7b04905f6349daa397430a34a63675
Jonah Snider <jonah@jonahsnider.com>
2021-12-01 22:22:27 -0800

    refactor(day2): clean up

--- src/days/implementations/2/index.ts
+++ src/days/implementations/2/index.ts
@@ -1,24 +1,21 @@
-import {lines} from '@jonahsnider/util';
+import assert from 'node:assert/strict';
+import {enumHas, lines} from '@jonahsnider/util';
 import {Day} from '../../../lib/solution.js';
 import type {SolutionPair} from '../../../lib/types.js';
 
 enum Direction {
-	Forward = 'forward',
-	Down = 'down',
 	Up = 'up',
+	Down = 'down',
+	Forward = 'forward',
+}
+interface Command {
+	direction: Direction;
+	units: number;
 }
 
 export class Day2 extends Day {
-	#checksum(position: {horizontal: number; depth: number}): number {
-		return position.horizontal * position.depth;
-	}
-
 	solve(input: string): SolutionPair {
-		const commands = lines(input).map(line => {
-			const split = line.split(' ');
-
-			return {units: Number(split[1]), direction: split[0] as Direction};
-		});
+		const commands = lines(input).map(line => this.#parseCommand(line));
 
 		const positions = {
 			part1: {
@@ -34,25 +31,42 @@ export class Day2 extends Day {
 
 		for (const command of commands) {
 			switch (command.direction) {
-				case Direction.Down:
-					positions.part1.depth += command.units;
-					positions.part2.aim += command.units;
-					break;
 				case Direction.Up:
 					positions.part1.depth -= command.units;
+
 					positions.part2.aim -= command.units;
+
+					break;
+				case Direction.Down:
+					positions.part1.depth += command.units;
+
+					positions.part2.aim += command.units;
+
 					break;
 				case Direction.Forward:
 					positions.part1.horizontal += command.units;
+
 					positions.part2.horizontal += command.units;
 					positions.part2.depth += positions.part2.aim * command.units;
 
 					break;
 				default:
-					throw new RangeError('Unknown direction');
+				// Unreachable
 			}
 		}
 
 		return [this.#checksum(positions.part1), this.#checksum(positions.part2)];
+	}
+
+	#parseCommand(command: string): Command {
+		const [direction, units] = command.split(' ');
+
+		assert(enumHas(Direction, direction), new RangeError(`Unknown direction: ${direction}`));
+
+		return {direction, units: Number(units)};
+	}
+
+	#checksum(position: {horizontal: number; depth: number}): number {
+		return position.horizontal * position.depth;
 	}
 }

dffa4898cbd8633c8500b7bf9c23db58b5848727
Jonah Snider <jonah@jonahsnider.com>
2021-12-07 15:36:54 -0800

    perf(day7): optimize

--- src/days/implementations/7/index.ts
+++ src/days/implementations/7/index.ts
@@ -1,33 +1,41 @@
-import {min, Sort} from '@jonahsnider/util';
 import {Day} from '../../../lib/solution.js';
 import type {SolutionPair} from '../../../lib/types.js';
 
 export class Day7 extends Day {
 	solve(input: string): SolutionPair {
+		const solution: SolutionPair = [Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY];
+
+		// Using a frequency table here is faster from a theoretical perspective
+		// Instead of processing [2, 2, 2, 2, 2] as 5 iterations you can do [[2, 5]] as 1 iteration
+		// In reality this adds about 6 milliseconds to the execution time with the actual input of 1000 numbers (~600 unique)
+		// For much larger input sizes you should use the frequency table approach
 		const positions = input.split(',').map(string => Number(string));
 
-		const distanceForPosition: Map<number, number> = new Map();
-		const fuelCostForPosition: Map<number, number> = new Map();
+		let minPosition = Number.POSITIVE_INFINITY;
+		let maxPosition = Number.NEGATIVE_INFINITY;
 
-		positions.sort(Sort.ascending);
-
-		const [minPosition] = positions;
-		const maxPosition = positions.at(-1)!;
-
-		for (let position = minPosition; position <= maxPosition; position++) {
-			for (const otherPosition of positions) {
-				const distance = this.distance(position, otherPosition);
-				distanceForPosition.set(position, (distanceForPosition.get(position) ?? 0) + distance);
-
-				const fuelCost = this.fuelCost(position, otherPosition);
-				fuelCostForPosition.set(position, (fuelCostForPosition.get(position) ?? 0) + fuelCost);
-			}
+		for (const position of new Set(positions)) {
+			minPosition = Math.min(minPosition, position);
+			maxPosition = Math.max(maxPosition, position);
 		}
 
-		const shortestDistance = [...distanceForPosition.values()].reduce(min);
-		const smallestFuelCost = [...fuelCostForPosition.values()].reduce(min);
+		for (let position = minPosition; position <= maxPosition; position++) {
+			let totalDistance = 0;
+			let totalFuelCost = 0;
 
-		return [shortestDistance, smallestFuelCost];
+			for (const otherPosition of positions) {
+				const distance = this.distance(position, otherPosition);
+				totalDistance += distance;
+
+				const fuelCost = this.fuelCost(position, otherPosition);
+				totalFuelCost += fuelCost;
+			}
+
+			solution[0] = Math.min(solution[0], totalDistance);
+			solution[1] = Math.min(solution[1], totalFuelCost);
+		}
+
+		return solution;
 	}
 
 	private distance(a: number, b: number): number {

0d718054cc2d4072d24eaf2f635e2c8e63e163a9
Jonah Snider <jonah@jonahsnider.com>
2021-12-06 16:20:07 -0800

    style: use single quotes

--- src/commands/template-command/template-files/part1.test.ts
+++ src/commands/template-command/template-files/part1.test.ts
@@ -1,7 +1,7 @@
 import test from 'ava';
 import {Day999} from './index.js';
 
-const input = `REPLACEME`;
+const input = 'REPLACEME';
 
 test('works', t => {
 	const [solution] = new Day999().solve(input);
